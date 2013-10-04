17159789d0f146f43fdb3bb1100f47e2fa8f469d
SupachokeBrahmavira <s6070130@kmitl.ac.th>
2013-10-04 16:11:42 +0700

    for check time update

--- poker.py
+++ poker.py
@@ -3,5 +3,6 @@ This is a first test on poker.py
 Prepare for coding . . .
 '''
 """ test update poker.py by Supachoke """
+""" test update poker.py again for check time update """
 
 ### test for study PSIT please delete before write code or somthing
