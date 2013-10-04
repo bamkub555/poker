c4778ea9d1ac4511b4444612239d145d709d43cc
bamkub555 <s6070104@kmitl.ac.th>
2013-10-04 23:07:35 +0700

    Fixed indent problem and added testcase for straight and flush

--- poker_test.py
+++ poker_test.py
@@ -1,17 +1,28 @@
-'''poker_test.py'''
 import unittest
 import poker
 
-class TestPoker(unittest.Testcase):
+class TestPoker(unittest.TestCase):
     '''Example unittest test methods for poker'''
 
     def test_poker_straight_flush(self):
-        '''Test get straightflush'''
+        '''Test get straight_flush'''
+        actual = poker.straight_flush(['JC', 'TC', '9C', '8C', '7C'])
+        expected = True
+        self.assertEqual(actual, expected)
+
+    def test_poker_straight(self):
+        '''Test get straight'''
+        actual = poker.straight(['JC', 'TC', '9C', '8C', '7C'])
+        expected = True
+        self.assertEqual(actual, expected)
+
+    def test_poker_flush(self):
+        '''Test get flush'''
+        actual = poker.flush(['JC', 'TC', '9C', '8C', '7C'])
+        expected = True
+        self.assertEqual(actual, expected)
 
         
-    actual = poker.straight_flush(['JC', 'TC', '9C', '8C', '7C'])
-    expected = 8
-    self.assertEqual(actual, expected)
 
 
 
