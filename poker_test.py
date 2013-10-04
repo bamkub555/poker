7617351bdcf19701938af6de66ca17c74b80a352
bamkub555 <s6070104@kmitl.ac.th>
2013-10-04 18:13:09 +0700

    Added straight_flush's testcase

--- poker_test.py
+++ poker_test.py
@@ -1,1 +1,19 @@
 '''poker_test.py'''
+import unittest
+import poker
+
+class TestPoker(unittest.Testcase)
+    '''Example unittest test methods for poker'''
+
+    def test_poker_straight_flush(self):
+        '''Test get straightflush'''
+
+        
+    actual = poker.straight_flush(['JC', 'TC', '9C', '8C', '7C'])
+    expected = 8
+    self.assertEqual(actual, expected)
+
+
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
