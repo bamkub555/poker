c2744c2f432361cc26c9f06feff54a80dc2c918b
SupachokeBrahmavira <s6070130@kmitl.ac.th>
2013-10-05 16:25:36 +0700

    Added 3kind and 2kind and fix : after 4 kind

--- poker_test.py
+++ poker_test.py
@@ -24,12 +24,23 @@ class TestPoker(unittest.TestCase):
         
     def test_poker_full_house(self):
         '''Test get full_house'''
-        actual = poker.fullhouse(['3', '3', '3', '4', '4'])
+        actual = poker.full_house([3, 3, 3, 4, 4])
         expected = True
         self.assertEqual(actual, expected)
-    def test_four_of_a_kind(self)
-        '''Test get four_of_a_kind'''
+        
+    def test_poker_four_of_kind(self):
+        '''Test get four_of_kind'''
         actual = poker.kind(4, [7,7,7,7,5])
+        expected = 7
+        self.assertEqual(actual, expected)
+    def test_poker_three_of_kind(self):
+        '''Test get three_of_kind'''
+        actual = poker.kind(3, [4,7,7,7,5])
+        expected = 7
+        self.assertEqual(actual, expected)
+    def test_poker_two_of_kind(self):
+        '''Test get two_of_kind'''
+        actual = poker.kind(2, [4,6,7,7,5])
         expected = 7
         self.assertEqual(actual, expected)
 
